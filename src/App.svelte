f0eed30a679ed9d0b71a112937cc39f15522dcc6
uragirimono00 <gudtj7578@gmail.com>
2025-02-22 21:48:32 +0900

    안올린 파일 전부 커밋

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<!-- src/App.svelte -->
+<script>
+    import { onAuthStateChanged } from "firebase/auth";
+    import { auth } from "./firebase.js";
+    import Login from "./Login.svelte";
+    import Signup from "./Signup.svelte";
+    import Dashboard from "./Dashboard.svelte";
+
+    let user = null;
+    let showSignup = false;
+
+    onAuthStateChanged(auth, (currentUser) => {
+        user = currentUser;
+    });
+
+    function handleAuthSuccess(event) {
+        user = event.detail.user;
+    }
+
+    function toggleSignup() {
+        showSignup = !showSignup;
+    }
+</script>
+
+{#if !user}
+    {#if showSignup}
+        <Signup on:authSuccess={handleAuthSuccess} />
+        <p>이미 계정이 있으신가요? <a href="#" on:click|preventDefault={() => showSignup = false}>로그인</a></p>
+    {:else}
+        <Login on:authSuccess={handleAuthSuccess} />
+        <p>계정이 없으신가요? <a href="#" on:click|preventDefault={() => showSignup = true}>회원가입</a></p>
+    {/if}
+{:else}
+    <Dashboard {user} />
+{/if}
